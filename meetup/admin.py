900292600e0134d8f3c2dc004e63f36169216295
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-11-27 20:55:10 +0300

    Добавила модели

--- meetup/admin.py
+++ meetup/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meetup, Speaker,Guest
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    search_fields = [
+        'number',
+        'tg_id',
+        'name',
+        'topic',
+    ]
+    list_display = [
+        'number',
+        'tg_id',
+        'name',
+        'topic',
+    ]
+    ordering = ['number']
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    search_fields = [
+        'tg_id',
+        'name',
+        'filled_in',
+    ]
+    list_display = [
+        'tg_id',
+        'name',
+        'filled_in',
+    ]
+
+@admin.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    search_fields = [
+        'beginning',
+        'time_limit',
+        'address',
+    ]
+    list_display = [
+        'beginning',
+        'time_limit',
+        'address',
+    ]
--- meetup/models.py
+++ meetup/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Speaker(models.Model):
+    tg_id = models.CharField(
+        verbose_name='Телеграмм ID',
+        max_length=50,
+        unique=True,
+    )
+    name = models.CharField(
+        verbose_name='Имя',
+        max_length=100,
+    )
+    topic = models.CharField(
+        verbose_name='Тема выступления',
+        max_length=200,
+    )
+    number = models.PositiveIntegerField(
+        unique=True,
+        null=True,
+        blank=True,
+        validators=[MinValueValidator(1)]
+    )
+
+    class Meta:
+        verbose_name = 'спикер'
+        verbose_name_plural = 'спикеры'
+
+    def __str__(self):
+        return self.name
+
+
+class Guest(models.Model):
+    tg_id = models.CharField(
+        verbose_name='Телеграмм ID',
+        max_length=50,
+        unique=True,
+    )
+    name = models.CharField(
+        verbose_name='Имя',
+        max_length=100,
+    )
+    filled_in = models.DateTimeField(
+        verbose_name='время заполнения',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'гость'
+        verbose_name_plural = 'гости'
+
+    def __str__(self):
+        return self.name
+
+
+class Meetup(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        blank=True,
+    )
+    beginning = models.DateTimeField(
+        verbose_name='Начало',
+    )
+    time_limit = models.PositiveIntegerField(
+        verbose_name='Регламент',
+        unique=True,
+        validators=[MinValueValidator(10)],
+    )
+    address = models.CharField(
+        verbose_name='Адрес',
+        max_length=200,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'встреча'
+        verbose_name_plural = 'встречи'
+
+    def __str__(self):
+        return self.name
