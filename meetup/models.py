47de38145983fc170cfabc074f9e203c195adbc1
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-11-28 21:49:33 +0300

    Обновила модели

--- meetup/models.py
+++ meetup/models.py
@@ -2,22 +2,27 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-class Guest(models.Model):
+class Participant(models.Model):
     tg_id = models.CharField(
         verbose_name='Телеграмм ID',
         max_length=50,
         unique=True,
     )
-    name = models.CharField(
+    user_name = models.CharField(
+        verbose_name='Ник',
+        max_length=50,
+        unique=True,
+    )
+    full_name = models.CharField(
         verbose_name='Имя',
         max_length=100,
     )
-    filled_in = models.DateTimeField(
+    filled_at = models.DateTimeField(
         verbose_name='время заполнения',
         auto_now_add=True,
     )
-    specialization = models.CharField(
-        verbose_name='Специализация',
+    stak = models.CharField(
+        verbose_name='Стек',
         max_length=100,
     )
     communication = models.BooleanField(
@@ -33,14 +38,9 @@ class Guest(models.Model):
         return self.name
 
 
-class Speaker(models.Model):
-    tg_id = models.CharField(
-        verbose_name='Телеграмм ID',
-        max_length=50,
-        unique=True,
-    )
-    name = models.ForeignKey(
-        Guest,
+class Speесн(models.Model):
+    speaker = models.ForeignKey(
+        Participant,
         verbose_name='Имя',
         on_delete=models.CASCADE,
     )
@@ -48,11 +48,10 @@ class Speaker(models.Model):
         verbose_name='Тема выступления',
         max_length=200,
     )
-    number = models.PositiveIntegerField(
+    ordinal = models.PositiveIntegerField(
         unique=True,
         null=True,
         blank=True,
-        validators=[MinValueValidator(1)]
     )
     time_limit = models.PositiveIntegerField(
         verbose_name='Время выступления',
@@ -65,38 +64,7 @@ class Speaker(models.Model):
         verbose_name_plural = 'спикеры'
 
     def __str__(self):
-        return self.name
-
-
-class Guest(models.Model):
-    tg_id = models.CharField(
-        verbose_name='Телеграмм ID',
-        max_length=50,
-        unique=True,
-    )
-    name = models.CharField(
-        verbose_name='Имя',
-        max_length=100,
-    )
-    filled_in = models.DateTimeField(
-        verbose_name='время заполнения',
-        auto_now_add=True,
-    )
-    specialization = models.CharField(
-        verbose_name='Специализация',
-        max_length=100,
-    )
-    communication = models.BooleanField(
-        verbose_name='Общение',
-        default=False,
-    )
-
-    class Meta:
-        verbose_name = 'гость'
-        verbose_name_plural = 'гости'
-
-    def __str__(self):
-        return self.name
+        return self.speaker.full_name
 
 
 class Meetup(models.Model):
