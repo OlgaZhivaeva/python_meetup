2789e54a908e855784831529ef21c24b21ee1072
paraigor <103633539+paraigor@users.noreply.github.com>
2024-12-02 12:27:18 +0300

    Update donate.py

--- telegram_bot/donate.py
+++ telegram_bot/donate.py
@@ -11,6 +11,7 @@ from telegram.ext import (
     ConversationHandler,
     Filters,
     MessageHandler,
+    PreCheckoutQueryHandler,
     Updater,
 )
 
@@ -46,21 +47,31 @@ def donate(update: Update, context: CallbackContext):
     if int(amount) < 10:
         update.message.reply_text("Сумма должна быть больше 10 руб.")
         return
-
+    context.user_data["donate_amount"] = int(amount)
     invoice_amount = amount + "00"
-    meetup = context.user_data["current_meetup"]
-    donor = context.user_data["participant"]
-    create_donation(meetup, donor, int(amount))
-
     context.bot.send_invoice(
         chat_id=update.effective_chat.id,
-        title="Оплата",
-        description="Сделать пожертвование",
+        title="Пожертвование",
+        description="Перевести",
         provider_token=settings.PAYMENT_TOKEN,
         currency="RUB",
         payload="donate",
-        prices=[LabeledPrice(label="title", amount=int(invoice_amount))],
+        prices=[
+            LabeledPrice(label="Пожертвование", amount=int(invoice_amount))
+        ],
     )
+
+
+def precheckout(update: Update, context: CallbackContext):
+    query = update.pre_checkout_query
+    query.answer(ok=True)
+
+
+def pay_success(update: Update, context: CallbackContext):
+    amount = context.user_data["donate_amount"]
+    meetup = context.user_data["current_meetup"]
+    donor = context.user_data["participant"]
+    create_donation(meetup, donor, amount)
 
 
 def to_menu(update: Update, context: CallbackContext):
@@ -72,18 +83,16 @@ def handlers_register(updater: Updater):
     updater.dispatcher.add_handler(
         ConversationHandler(
             entry_points=[
-                CallbackQueryHandler(
-                    amount_request, pattern="^donate$"
-                )
+                CallbackQueryHandler(amount_request, pattern="^donate$")
             ],
             states={
                 "DONATE": [
-                    MessageHandler(
-                        Filters.text & ~Filters.command, donate
-                    ),
+                    MessageHandler(Filters.text & ~Filters.command, donate),
+                    MessageHandler(Filters.successful_payment, pay_success),
                 ]
             },
             fallbacks=[CallbackQueryHandler(to_menu, pattern="^to_menu$")],
         )
     )
+    updater.dispatcher.add_handler(PreCheckoutQueryHandler(precheckout))
     return updater.dispatcher
